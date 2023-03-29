5b53ccc70a099117c4f39a0dd966a4468c63efbf
Karl St-Arnaud <kastaa@users.noreply.github.com>
2023-03-28 22:41:00 -0400

    main function to process data

---
+++ roadqa_imu/main.py
@@ -0,0 +1,31 @@
+import argparse
+import webbrowser
+from pathlib import Path
+
+from roadqa_imu.imu_gps_data import ImuGpsData
+from roadqa_imu.roadqa_map import RoadQAMap
+from roadqa_imu.utils import open_csv
+
+
+def main(data_path):
+    data_buffer = open_csv(data_path)
+    imu_gps_data = ImuGpsData.from_csv(data_buffer)
+    
+    imu_gps_data.process()
+    road_map = RoadQAMap(imu_gps_data)
+    
+    map = road_map.generate_map()
+    
+    output_file = "map_result.html"
+    map.save(output_file)
+    webbrowser.open(output_file, new=1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_path", type=Path, help="Data path")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args.data_path)
+    main(args.data_path)
