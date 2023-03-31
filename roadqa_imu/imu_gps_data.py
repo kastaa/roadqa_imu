365b805672dcc588cc36e3b020951931c621683e
Karl St-Arnaud <kastaa@users.noreply.github.com>
2023-03-30 23:13:40 -0400

    Adding docstring

--- roadqa_imu/imu_gps_data.py
+++ roadqa_imu/imu_gps_data.py
@@ -7,6 +7,8 @@ from .utils import interpolate_data
 
 @dataclass
 class IMU:
+    """IMU data as collected by the measuring device
+    """
     ms_count: int
     acc_x: float
     acc_y: float
@@ -14,6 +16,8 @@ class IMU:
 
 @dataclass
 class GPS:
+    """GPS data as collected by the measuring device
+    """
     ms_count: int
     latitude: float
     longitude: float 
@@ -21,7 +25,7 @@ class GPS:
 
 
 class ImuGpsData:
-    metadata_index = 2
+    metadata_index = 2 # number of medata line in the data file
     
     def __init__(self, gps: List[GPS] = [], imu: List[IMU] = []):
         self._raw_gps = gps
@@ -33,6 +37,17 @@ class ImuGpsData:
         
     @classmethod
     def from_csv(cls, csv_buffer: List[str]):
+        """Initalize the ImuGpsData from a data file.
+
+        Args:
+            csv_buffer (List[str]): list of line from the data file.
+
+        Raises:
+            ValueError: unknown data in the data file.
+
+        Returns:
+            _type_: _description_
+        """
         # TODO make some validation using the metadata. Eventually if I do update, the reader could be based
         # on what in those metadata
         data_dict = {"gps": [], "imu": []}
@@ -50,17 +65,24 @@ class ImuGpsData:
                 raise ValueError(f"Got an unexpected sensor data {sensor_key}")
         return cls(**data_dict)
         
-    def process(self, interpolation_period_ms=10):
+    def process(self, interpolation_period_ms=10) -> None:
+        """Process IMU and GPS data to return a combine version of both data on the 
+        same time axis.
+
+        Args:
+            interpolation_period_ms (int, optional): Sampling period of combine data. 
+            Defaults to 10.
+        """
         self._find_valid_gps()
         self._find_common_time_axis(interpolation_period_ms=interpolation_period_ms)
         self._process_gps()
         self._process_imu()
     
-    def _find_valid_gps(self):
+    def _find_valid_gps(self) -> None:
         valid = [np.abs(gps.latitude) > 0 and np.abs(gps.longitude) > 0 for gps in self._raw_gps]
         self._valid_gps = [index for (index, valid_value) in enumerate(valid) if valid_value]
 
-    def _find_common_time_axis(self, interpolation_period_ms: int = 10):
+    def _find_common_time_axis(self, interpolation_period_ms: int = 10) -> None:
         ms_count_gps = np.uint64([gps.ms_count for gps in self._raw_gps])[self._valid_gps]
         ms_count_imu = np.uint64([imu.ms_count for imu in self._raw_imu])
         
@@ -70,7 +92,7 @@ class ImuGpsData:
         
         self.ms_count = np.arange(min_common_time, max_common_time, interpolation_period_ms)
         
-    def _process_imu(self):
+    def _process_imu(self) -> None:
         self.acceleration = []
         ms_count_imu = np.uint64([imu.ms_count for imu in self._raw_imu])
         acceleration_data = np.float32([[imu.acc_x, imu.acc_y, imu.acc_z] for imu in self._raw_imu])
@@ -79,7 +101,7 @@ class ImuGpsData:
             self.acceleration.append(acc_component_interpolate)
         self.acceleration = np.float32(self.acceleration)
     
-    def _process_gps(self):
+    def _process_gps(self) -> None:
         self.coord = []
         ms_count_gps = np.uint64([gps.ms_count for gps in self._raw_gps])[self._valid_gps]
         gps_data = np.float32([[gps.latitude, gps.longitude] for gps in self._raw_gps])
--- roadqa_imu/roadqa_map.py
+++ roadqa_imu/roadqa_map.py
@@ -6,17 +6,38 @@ from scipy.ndimage import generic_filter
 
 from .imu_gps_data import ImuGpsData
 
+SENSIBILITY_THRESHOLD = 0.1
+
 
 class RoadQAMap:
-    def __init__(self, imu_gps_data: ImuGpsData, map_type: str = "stamentoner", map_zoom: int = 15,
-                 colormap: str = "jet"):
+    """Class that generate vibration map from ImuGPSData input."""
+    def __init__(self, imu_gps_data: ImuGpsData, map_type: str = "stamentoner", 
+                 map_zoom: int = 15, colormap: str = "jet") -> None:
+        """
+
+        Args:
+            imu_gps_data (ImuGpsData): Imu ang GPS data
+            map_type (str, optional): map type used to draw the map. Defaults 
+            to "stamentoner".
+            map_zoom (int, optional): Zoom used to draw the map. Defaults to 15.
+            colormap (str, optional): Colormap identifier to draw vibration data. 
+            Defaults to "jet".
+        """
         self.imu_gps_data = imu_gps_data
         self.process_vibration()
         self._map_type = map_type
         self._map_zoom = map_zoom
         self._color_map = self._generate_colormap(colormap)
     
-    def _generate_colormap(self, colormap: str):
+    def _generate_colormap(self, colormap: str) -> dict[int, str]:
+        """Generate a color map dictionnary used to display the vibration data
+
+        Args:
+            colormap (str): Color map identifier.
+
+        Returns:
+            dict[int, str]: color map dictionnary.
+        """
         cmap = cm.get_cmap(colormap, 255)
         color_dict = {}
         for i in range(cmap.N):
@@ -25,25 +46,47 @@ class RoadQAMap:
         return color_dict
         
     def process_vibration(self, threshold: float = 0.01, sensibility: float = 0.4, 
-                          contrast: float = (1 / 3.)):
+                          contrast: float = (1 / 3.)) -> None:
+        """Process IMU data to generate a estimate a quality estimation of the road 
+
+        Args:
+            threshold (float, optional): Threshold value between 0 and 1 at which 
+            vibration are rounded to 0. Defaults to 0.01.
+            sensibility (float, optional): Sensibility factor between 0 and 1 that 
+            increase the sensibility of the metric to small vibration. Defaults to 0.4.
+            contrast (float, optional): Constrast factor between 0 and 1 to increase 
+            contrast of the metric. Defaults to (1 / 3.).
+        """
+        # Only taking the up/down component of acceleration
         acc_y = self.imu_gps_data.acceleration[2, :]
         acc_y_norm = acc_y - acc_y.mean()
-        diff_acc_y = np.abs(np.diff(acc_y))
+        diff_acc_y = np.abs(np.diff(acc_y_norm))
         
         vibration = diff_acc_y - diff_acc_y.min()
-        vibration = (vibration / vibration.max()) ** (1 / sensibility)
+        sensibility = sensibility + SENSIBILITY_THRESHOLD
+        vibration = (vibration / vibration.max()) ** (1 / (sensibility))
 
         vibration[vibration < threshold] = vibration[vibration < threshold] * 0
 
         self.vibration = vibration ** contrast
     
-    def _create_map(self):
+    def _create_map(self) -> None:
+        """Initialize / reset the map"""
         coord_mean = self.imu_gps_data.coord.mean(axis=1)
         init_map = folium.Map(location=coord_mean, zoom_start=self._map_zoom)
         folium.TileLayer(self._map_type).add_to(init_map)
         return init_map
     
     def generate_map(self, subsample_factor: int = 100):
+        """Display the vibration data on the map 
+
+        Args:
+            subsample_factor (int, optional): vibration data are subsample on the map by this 
+            factor. Defaults to 100.
+
+        Returns:
+            _type_: _description_
+        """
         map_ = self._create_map()
         
         vibration_max = generic_filter(self.vibration, np.max, size=subsample_factor)
